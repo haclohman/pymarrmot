5c23b0138325ac6a4e746d1ee4c447aff7f5670f
ssheeder_rtiintl <ssheeder@rti.org>
2025-07-18 08:57:14 -0400

    Committing all changes made to models, and functions. Model tests are not pushed due to file count/size. I have tests locally. Please reach out if you want these tests.

--- src/pymarrmot/Functions/autocalibration/spotpy_setup.py
+++ src/pymarrmot/Functions/autocalibration/spotpy_setup.py
@@ -4,7 +4,6 @@ from spotpy.objectivefunctions import rmse
 
 from pymarrmot.models.models.m_29_hymod_5p_5s import m_29_hymod_5p_5s
 
-import os
 import pandas as pd
 import numpy as np
 
@@ -33,7 +32,7 @@ class spotpy_setup(object):
     def __init__(self,obj_func=None):
         self.obj_func = obj_func
         
-        #initial storage values - set as average of the range
+    #initial storage values - set as average of the range
     input_s0 = []
     #loop over par_ranges dictionary and get average value for each key
     for key in par_ranges:
@@ -49,21 +48,29 @@ class spotpy_setup(object):
     m = m_29_hymod_5p_5s()
     m.delta_t = 1
 
-    df = pd.read_csv('C:/Users/ssheeder/Repos/pymarrmot/examples/Example_DataSet_NewDateFormat.csv')
+    df = pd.read_csv('C:/Users/ssheeder/Repos/pymarrmot/model_tests_hourly/forcing/forcing_daily_04288000.csv')
     # Create a climatology data input structure
     input_climatology = {
-        'dates': df['date'].to_numpy(),      # Daily data: date in 'm/d/yyyy' format
-        'precip': df['precip'].to_numpy(),   # Daily data: P rate [mm/d]
-        'temp': df['temp'].to_numpy(),       # Daily data: mean T [degree C]
-        'pet': df['pet'].to_numpy(),         # Daily data: Ep rate [mm/d]
+        'dates': df['value_time'].to_numpy(),      # Daily data: date in 'm/d/yyyy' format
+        'precip': df['ppt_mm'].to_numpy(),   # Daily data: P rate [mm/d]
+        'temp': df['temp_c'].to_numpy(),       # Daily data: mean T [degree C]
+        'pet': df['pet_mm'].to_numpy(),         # Daily data: Ep rate [mm/d]
     }
-    trueObs = df['Q'].to_list()
+    trueObs = df['obs_mm/day'].to_list() # Daily data: Q rate [cms]
+
+    # Clean up the evaluation and simulation data - remove any NaNs and missing values from evaluation dataset, and corresponding values from simulation dataset
+    nan_indices = np.argwhere(np.isnan(trueObs))
+    #missing_indices = np.argwhere(trueObs == "")
+    #unique_values = list(set(nan_indices + missing_indices))
+    if len(nan_indices) > 0:
+        print(f"WARNING: {len(nan_indices)} NaN and missing values found in evaluation dataset. These values will be removed from the evaluation and simulation datasets before calculating model fitness.")
+        trueObs = np.delete(trueObs, nan_indices)
+
     m.input_climate = input_climatology
     
     m.solver_opts = input_solver_opts
     m.S0 = np.array(input_s0)
         
-  
     # Step 3: Write the def simulation function, which starts your model and returns the results
     def simulation(self,x):
         #update theta with the new parameter set
@@ -71,7 +78,10 @@ class spotpy_setup(object):
         
         #Here the model is actualy started with a unique parameter combination that it gets from spotpy for each time the model is called
         (output_ex, output_in, output_ss, output_waterbalance) = self.m.get_output(nargout=4)
-        return output_ex['Q'].tolist()
+        sim_q = output_ex['Q']
+        if self.nan_indices.size > 0:
+            sim_q = np.delete(sim_q, self.nan_indices)
+        return sim_q.tolist()
     
     # Step 4: Write the def evaluation function, which returns the observations
     def evaluation(self):
@@ -81,7 +91,7 @@ class spotpy_setup(object):
     def objectivefunction(self, simulation, evaluation, params=None):
     #SPOTPY expects to get one or multiple values back, 
     #that define the performance of the model run
-        
+       
         if not self.obj_func:
             # This is used if not overwritten by user
             score = rmse(evaluation, simulation)
@@ -96,10 +106,4 @@ class spotpy_setup(object):
             score2 = self.obj_func(eval_inverse, sim_inverse)
             result = (score + score2)/2
             like = -1*result
-
-            # Calculation of kge 
-            # result = self.obj_func(evaluation, simulation)
-            # like = -1*result
-
-
         return like
